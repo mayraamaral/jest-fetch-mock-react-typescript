1ad628d4e7ac1618cc1207d10446d94859ed4490
Mayra Amaral <mayracunha18@gmail.com>
2023-03-16 20:21:49 -0300

    refactor: updates readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,24 +3,21 @@ import fetchMock from "jest-fetch-mock";
 import App from "./App";
 import { fakeUsers } from "./utils/mockData";
 
-describe("testa a página principal", () => {
+describe("testing main page", () => {
   beforeEach(() => {
     fetchMock.resetMocks();
   });
 
-  test("renderiza usuários", async () => {
+  test("render mocked users", async () => {
     fetchMock.mockResponse(JSON.stringify(fakeUsers));
 
     render(<App />);
-
-    //const mockedUser = await screen.findAllByText(/mayra/i);
-    //expect(mockedUser).toBeInTheDocument();
 
     for (let i = 0; i < fakeUsers.length; i++) {
       const mockedUser = await screen.findByText(fakeUsers[i].name);
       expect(mockedUser).toBeInTheDocument();
     }
 
-    screen.logTestingPlaygroundURL();
+    //screen.logTestingPlaygroundURL();
   });
 });
